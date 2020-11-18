6084338bca8c01bde697af128bc624f2f894fa27
morganjmccombs <morganjmccombs@yahoo.com>
2020-11-17 19:07:22 -0500

    changed the objective to returns, made a folder for training data

--- module_numericFinancialData.py
+++ module_numericFinancialData.py
@@ -1,11 +1,12 @@
 
-from import_my_packages import * 
+from import_my_packages import *
 from datetime import timedelta
+import datetime
 
 def get_daily_historical_stock_price_specific_statement(ticker, start_date, end_date, info_type):
   url_to_open = 'https://finance.yahoo.com/quote/' + ticker + '/'+ info_type + '?p=' + ticker
   print(url_to_open)
-  read_data = ur.urlopen(url_to_open).read() 
+  read_data = ur.urlopen(url_to_open).read()
   soup= BeautifulSoup(read_data,'lxml')
   features = soup.find_all('div', class_='D(tbr)')
 
@@ -29,14 +30,17 @@ def get_daily_historical_stock_price_specific_statement(ticker, start_date, end_
   df = pd.DataFrame(final[1:])
   df.columns = headers
 
+
   def convert_to_numeric(column): #function to make all values numerical
       final_col = pd.to_numeric([i.replace('-','') for i in [i.replace(',','') for i in column]])
       return final_col
-  
+
   for column in headers[1:]:
     df[column] = convert_to_numeric(df[column])
     final_df = df.fillna('-')
   return(final_df)
+
+
 
 def get_daily_historical_stock_price(ticker, start_date, end_date):
   output = []
@@ -56,85 +60,21 @@ def get_historic_stock_prices(ticker, start_date, end_date):
 
 
 
+def get_FFM5(start_date, end_date):
+  df = pd.read_csv("ffm5factorsdaily.csv")
+  df['Date'] = df['Date'].astype(str)
+  df['year'] = df['Date'].str[0:4]
+  df['month'] = df['Date'].str[4:6]
+  df['day'] = df['Date'].str[6:]
+  df['datestring'] = df['year'] + df['month'] + df['day']
+  df['Date_Column'] = pd.to_datetime(df['datestring'], infer_datetime_format=True)
+  df = df[df['Date_Column'] >= start_date]
+  df = df[df['Date_Column'] <= end_date]
+  df.drop(['Date', 'year', 'month', 'day', 'datestring'], inplace = True, axis = 1)
+  return(df)
 
 
-### Fama French Factors ###
-def get_Fama_French_Factors(ticker, start_date, end_date):
 
-  # Define Helper Function 1 #
-  def price2ret(prices,retType='simple'):
-      if retType == 'simple':
-          ret = (prices/prices.shift(1))-1
-      else:
-          ret = np.log(prices/prices.shift(1))
-      return ret
-  # Define Helper Function 2 #
-  def assetPriceReg(df_stk):
-      import pandas_datareader.data as web  # module for reading datasets directly from the web
-      
-      # Reading in factor data
-      df_factors = web.DataReader('F-F_Research_Data_5_Factors_2x3_daily', 'famafrench')[0]
-      
-      df_factors.rename(columns={'Mkt-RF': 'MKT'}, inplace=True)
-      df_factors['MKT'] = df_factors['MKT']/100 
-      df_factors['SMB'] = df_factors['SMB']/100
-      df_factors['HML'] = df_factors['HML']/100
-      df_factors['RMW'] = df_factors['RMW']/100
-      df_factors['CMA'] = df_factors['CMA']/100
-      
-      df_stock_factor = pd.merge(df_stk,df_factors,left_index=True,right_index=True) # Merging the stock and factor returns dataframes together
-      df_stock_factor['XsRet'] = df_stock_factor['Returns'] - df_stock_factor['RF'] # Calculating excess returns
-
-      # Running CAPM, FF3, and FF5 models.
-      CAPM = smf.ols(formula = 'XsRet ~ MKT', data=df_stock_factor).fit(cov_type='HAC',cov_kwds={'maxlags':1})
-      FF3 = smf.ols( formula = 'XsRet ~ MKT + SMB + HML', data=df_stock_factor).fit(cov_type='HAC',cov_kwds={'maxlags':1})
-      FF5 = smf.ols( formula = 'XsRet ~ MKT + SMB + HML + RMW + CMA', data=df_stock_factor).fit(cov_type='HAC',cov_kwds={'maxlags':1})
-
-      CAPMtstat = CAPM.tvalues
-      FF3tstat = FF3.tvalues
-      FF5tstat = FF5.tvalues
-
-      CAPMcoeff = CAPM.params
-      FF3coeff = FF3.params
-      FF5coeff = FF5.params
-
-      # DataFrame with coefficients and t-stats
-      results_df = pd.DataFrame({'CAPMcoeff':CAPMcoeff,'CAPMtstat':CAPMtstat,
-                                'FF3coeff':FF3coeff, 'FF3tstat':FF3tstat,
-                                'FF5coeff':FF5coeff, 'FF5tstat':FF5tstat},
-      index = ['Intercept', 'MKT', 'SMB', 'HML', 'RMW', 'CMA'])
-
-
-      dfoutput = summary_col([CAPM,FF3, FF5],stars=True,float_format='%0.4f',
-                    model_names=['CAPM','FF3','FF5'],
-                    info_dict={'N':lambda x: "{0:d}".format(int(x.nobs)),
-                              'Adjusted R2':lambda x: "{:.4f}".format(x.rsquared_adj)}, 
-                              regressor_order = ['Intercept', 'MKT', 'SMB', 'HML', 'RMW', 'CMA'])
-
-      return(pd.DataFrame(results_df))
-
-  # Begin Current Function
-  df = get_historic_stock_prices(ticker, start_date, end_date)
-  df['Returns'] = price2ret(df[['STOCK_PRICE_Adj_Close']])
-  df_regOutput = assetPriceReg(df)
-  
-  ### Test Run ###
-  fama_french_df = df_regOutput
-  capm_intercept = fama_french_df.at['Intercept', 'CAPMcoeff']
-  capm_MKT = fama_french_df.at['MKT', 'CAPMcoeff']
-  ff3_intercept = fama_french_df.at['Intercept', 'FF3coeff']
-  ff3_mkt = fama_french_df.at['MKT', 'FF3coeff']
-  ff3_smb = fama_french_df.at['SMB', 'FF3coeff']
-  ff5_intercept = fama_french_df.at['Intercept', 'FF5coeff']
-  ff5_mkt = fama_french_df.at['MKT', 'FF5coeff']
-  ff5_smb = fama_french_df.at['SMB', 'FF5coeff']
-  ff5_HML = fama_french_df.at['HML', 'FF5coeff']
-  ff5_RMW = fama_french_df.at['RMW', 'FF5coeff']
-
-  CAPM = [capm_intercept, capm_MKT]
-  FF3 = [ff3_intercept, ff3_mkt, ff3_smb]
-  FF5 = [ff5_intercept, ff5_mkt, ff5_smb, ff5_HML, ff5_RMW]
-  return(CAPM, FF3, FF5)
 
 def get_vix_index(start_date, end_date):
   end_date = end_date + timedelta(1)
@@ -143,6 +83,8 @@ def get_vix_index(start_date, end_date):
   df.drop("Volume", inplace = True, axis = 1)
   df.columns = ["VIX_" + i.replace(" ", "_") for i in df.columns]
   return(df)
+
+
 
 def get_composite_indices(start_date, end_date):
   NASDAQ_data = yf.download("^IXIC", start=start_date, end=end_date) # NASDAQ composite
@@ -166,33 +108,6 @@ def get_composite_indices(start_date, end_date):
   return( df)
 
 
-def get_historical_options_data(ticker, start_date, end_date):
-  return(1)
-
-
-
-def create_numeric_training_data(ticker, start_date, end_date):
-  ### Historic Stock Prices ###
-  historic_stock_prices = get_historic_stock_prices(ticker, start_date, end_date)
-
-  ### VIX ###
-  vix_df = get_vix_index(start_date, end_date)
-
-  ### Composite - Dow, Nasdaq, S&P ###
-  composite_df = get_composite_indices(start_date, end_date)
-
-  ### Financial Statements ###
-  income_statement_df, balance_sheet_df, cash_flow_statement_df = get_daily_historical_stock_price(ticker, start_date, end_date)
-
-  ### Fama French ###
-  fama_french_factors_CAPM, fama_french_factors_FF3, fama_french_factors_FF5 = get_Fama_French_Factors(ticker, start_date, end_date)
-
-  ### Merge ###
-  merged_df = pd.merge(historic_stock_prices,vix_df, how='outer', left_index=True, right_index=True)
-  merged_df = pd.merge(merged_df,composite_df, how='outer', left_index=True, right_index=True)
-  return(merged_df)
-
-
 
 
 def create_numeric_training_data(ticker, start_date, end_date):
@@ -205,37 +120,26 @@ def create_numeric_training_data(ticker, start_date, end_date):
   # ### Composite - Dow, Nasdaq, S&P ###
   composite_df = get_composite_indices(start_date, end_date)
 
-  # ### Financial Statements ###
-  # income_statement_df, balance_sheet_df, cash_flow_statement_df = get_daily_historical_stock_price(ticker, start_date, end_date)
-
   ### Fama French ###
-  fama_french_df_big = pd.DataFrame(columns = ['capm_intercept', 'capm_MKT', 
-                                           'ff3_intercept', 'ff3_mkt', 'ff3_smb', 
-                                           'ff5_intercept', 'ff5_mkt', 'ff5_smb', 'ff5_HML', 'ff5_RMW'])
-  # early_start_date = datetime.datetime(2018, 10, 30)
-  early_start_date = datetime(2018, 10, 30)
-
-  day_count = (end_date - start_date).days + 1
-  n = 0
-  for end_single_date in historic_stock_prices.index:
-    fama_french_factors_CAPM, fama_french_factors_FF3, fama_french_factors_FF5 = get_Fama_French_Factors(ticker, early_start_date, end_single_date)
-    fama_french_df = pd.DataFrame([[historic_stock_prices.index[n], fama_french_factors_CAPM[0], fama_french_factors_CAPM[1], 
-                                  fama_french_factors_FF3[0],fama_french_factors_FF3[1], fama_french_factors_FF3[2], 
-                                  fama_french_factors_FF5[0], fama_french_factors_FF5[1], fama_french_factors_FF5[2], fama_french_factors_FF5[3], fama_french_factors_FF5[4]]], 
-                                columns = ['Date_Column','capm_intercept', 'capm_MKT', 
-                                           'ff3_intercept', 'ff3_mkt', 'ff3_smb', 
-                                           'ff5_intercept', 'ff5_mkt', 'ff5_smb', 'ff5_HML', 'ff5_RMW'])
-    fama_french_df_big = fama_french_df_big.append(fama_french_df)
-    n += 1
-  print(fama_french_df_big)
+  fama_french_df_big = get_FFM5(start_date, end_date)
 
   ### Merge ###
   merged_df = pd.merge(historic_stock_prices,vix_df, how='outer', left_index=True, right_index=True)
   merged_df = pd.merge(merged_df,composite_df, how='outer', left_index=True, right_index=True)
   merged_df['Date_Column'] = merged_df.index
   merged_df = pd.merge(merged_df, fama_french_df_big, how='outer',left_on = 'Date_Column', right_on = 'Date_Column')#, right_on = 'Date_Column')# left_index=True, right_index=True)
-  merged_df['TARGET'] = merged_df['STOCK_PRICE_Open'].copy().shift(-1)
+  merged_df = merged_df.set_index('Date_Column')
+
+  for f in ['Mkt-RF', 'SMB', 'HML', 'RMW', 'CMA', 'RF']:
+    merged_df[f] = merged_df[f].copy().shift(1)
+
+  ### Edited after we decided to predict return ###
+  merged_df['TARGET'] = (merged_df['STOCK_PRICE_Open'] - merged_df['STOCK_PRICE_Close']) / (merged_df['STOCK_PRICE_Open'])
+
+  cols_to_keep = ['TARGET', 'STOCK_PRICE_Open','VIX_Open', 'NASDAQ_Open', 'DOW_Open', 'SP_Open', 'Mkt-RF', 'SMB', 'HML', 'RMW', 'CMA', 'RF']
+  merged_df = merged_df[cols_to_keep]
 
   merged_df.dropna(inplace = True, axis = 0)
-  merged_df = merged_df.set_index('Date_Column')
+  print(merged_df)
+
   return(merged_df)
--- run_MakeDataFrames.py
+++ run_MakeDataFrames.py
@@ -1,5 +1,6 @@
 from module_scraper import *
 from module_numericFinancialData import *
+
 from import_my_packages import *
 from module_featurizer import *
 from module_linregHelper import *
@@ -22,15 +23,15 @@ tick_size = 30
 ###################################################################
 
 
-# tickers = ['AMZN', 'GEr', 'PFE', 'T', 'GS']
-tickers = ['PFE', 'T', 'GS']
+tickers = ['AMZN', 'GE', 'PFE', 'T', 'GS']
+
+# tickers = ['AMZN']
 #------------------------------------## SPECIFY SETTINGS ##------------------------------------#
 
 
-# ticker = 'AMZN'
-start_date = datetime(2019, 10, 30)
+start_date = datetime(2012, 9, 30)
 start_date_string = str(start_date.month) + "/" + str(start_date.day) + "/" + str(start_date.year)
-end_date = datetime(2020, 10,30)
+end_date = datetime(2020, 9, 30)
 end_date_string = str(end_date.month) + "/" + str(end_date.day) + "/" + str(end_date.year)
 
 
@@ -41,43 +42,36 @@ def main(type_of_data_arg, ticker_ARG):
 
     home_directory = os.getcwd()
 
-    try:
-        X = pd.read_csv(home_directory + "/" + type_of_data_arg + "_training_data_" + ticker_ARG + "_" + "_".join(start_date_string.split("/")) + "_" + "_".join(end_date_string.split("/"))  + ".csv", index_col = 0)
-    except:
-        print("DF requested does not yet exist. Writing new data now...")
-        if type_of_data_arg == "numeric":
+    if type_of_data_arg == "numeric":
+        training_numeric_df = create_numeric_training_data(ticker_ARG, start_date, end_date)
+        training_numeric_df.to_csv(home_directory + "/DataCSVs/" + type_of_data_arg + "_training_data_" + ticker_ARG + "_" + "_".join(start_date_string.split("/")) + "_" + "_".join(end_date_string.split("/"))  + ".csv")
+
+    elif type_of_data_arg == "numeric_and_text":
+
+        ### Write numeric data ###
+        try: # if file already exists
+            training_numeric_df = pd.read_csv(home_directory + "/DataCSVs/numeric_training_data_" + ticker_ARG + "_" + "_".join(start_date_string.split("/")) + "_" + "_".join(end_date_string.split("/"))  + ".csv", index_col = 0)
+        except:
             training_numeric_df = create_numeric_training_data(ticker_ARG, start_date, end_date)
-            training_numeric_df.to_csv(home_directory + "/" + type_of_data_arg + "_training_data_" + ticker_ARG + "_" + "_".join(start_date_string.split("/")) + "_" + "_".join(end_date_string.split("/"))  + ".csv")
+            training_numeric_df.to_csv(home_directory + "/DataCSVs/" + "numeric" + "_training_data_" + ticker_ARG + "_" + "_".join(start_date_string.split("/")) + "_" + "_".join(end_date_string.split("/"))  + ".csv")
+            training_numeric_df = pd.read_csv(home_directory + "/DataCSVs/numeric_training_data_" + ticker_ARG + "_" + "_".join(start_date_string.split("/")) + "_" + "_".join(end_date_string.split("/"))  + ".csv", index_col = 0)
 
-        elif type_of_data_arg == "numeric_and_text":
-
-            ### Write numeric data ###
-            try: # if file already exists
-                training_numeric_df = pd.read_csv(home_directory + "/numeric_training_data_" + ticker_ARG + "_" + "_".join(start_date_string.split("/")) + "_" + "_".join(end_date_string.split("/"))  + ".csv", index_col = 0)
-            except:
-                training_numeric_df = create_numeric_training_data(ticker_ARG, start_date, end_date)
-                training_numeric_df.to_csv(home_directory + "/" + "numeric" + "_training_data_" + ticker_ARG + "_" + "_".join(start_date_string.split("/")) + "_" + "_".join(end_date_string.split("/"))  + ".csv")
-                training_numeric_df = pd.read_csv(home_directory + "/numeric_training_data_" + ticker_ARG + "_" + "_".join(start_date_string.split("/")) + "_" + "_".join(end_date_string.split("/"))  + ".csv", index_col = 0)
-
-            ### Write text data ###
-            try: # if file already exists
-                training_text_df = pd.read_csv(home_directory + "/text_training_data_" + ticker_ARG + "_" + "_".join(start_date_string.split("/")) + "_" + "_".join(end_date_string.split("/"))  + ".csv", index_col = 0)
-            except:
+        ### Write text data ###
+        try: # if file already exists
+            training_text_df = pd.read_csv(home_directory + "/DataCSVs/text_training_data_" + ticker_ARG + "_" + "_".join(start_date_string.split("/")) + "_" + "_".join(end_date_string.split("/"))  + ".csv", index_col = 0)
+        except:
 ###### NEED TO FIX TEXTUAL FEATURES HERE ####
-                training_text_data = pd.DataFrame([['88888888', '99999999' ] for i in range(len(training_numeric_df.index))], columns = ['textual_feature_1', 'textual_feature_2']) # NEED TO SPECIFY SOME FUNCTION HERE
-                training_text_data.index = training_numeric_df.index
+            training_text_data = pd.DataFrame([['88888888', '99999999' ] for i in range(len(training_numeric_df.index))], columns = ['textual_feature_1', 'textual_feature_2']) # NEED TO SPECIFY SOME FUNCTION HERE
+            training_text_data.index = training_numeric_df.index
 ###### NEED TO FIX TEXTUAL FEATURES HERE ####
-                training_text_data.to_csv(home_directory + "/" + "text" + "_training_data_" + ticker_ARG + "_" + "_".join(start_date_string.split("/")) + "_" + "_".join(end_date_string.split("/"))  + ".csv")
-                training_text_df = pd.read_csv(home_directory + "/text_training_data_" + ticker_ARG + "_" + "_".join(start_date_string.split("/")) + "_" + "_".join(end_date_string.split("/"))  + ".csv", index_col = 0)
+            training_text_data.to_csv(home_directory + "/DataCSVs/" + "text" + "_training_data_" + ticker_ARG + "_" + "_".join(start_date_string.split("/")) + "_" + "_".join(end_date_string.split("/"))  + ".csv")
+            training_text_df = pd.read_csv(home_directory + "/DataCSVs/text_training_data_" + ticker_ARG + "_" + "_".join(start_date_string.split("/")) + "_" + "_".join(end_date_string.split("/"))  + ".csv", index_col = 0)
 
-            ### Create joint df ###
-            training_numeric_and_text_df = pd.merge(training_numeric_df, training_text_df, how='outer', left_index=True, right_index=True)
-            training_numeric_and_text_df.to_csv(home_directory + "/" + type_of_data_arg + "_training_data_" + ticker_ARG + "_" + "_".join(start_date_string.split("/")) + "_" + "_".join(end_date_string.split("/"))  + ".csv")
+        ### Create joint df ###
+        training_numeric_and_text_df = pd.merge(training_numeric_df, training_text_df, how='outer', left_index=True, right_index=True)
+        training_numeric_and_text_df.to_csv(home_directory + "/DataCSVs/" + type_of_data_arg + "_training_data_" + ticker_ARG + "_" + "_".join(start_date_string.split("/")) + "_" + "_".join(end_date_string.split("/"))  + ".csv")
 
-        X = pd.read_csv(home_directory + "/" + type_of_data_arg + "_training_data_" + ticker_ARG + "_" + "_".join(start_date_string.split("/")) + "_" + "_".join(end_date_string.split("/"))  + ".csv", index_col = 0)
 
-    print("Entire X DF:")
-    print(X)
 
 
 if __name__ == "__main__":
@@ -88,7 +82,6 @@ if __name__ == "__main__":
     for ticker in tickers:
         main("numeric", ticker)
         # main("numeric_and_text", ticker)
-
 
 
 
